296b58f081d0a948fae3fc6df0a8d5a226f6f890
Dessalines <tyhou13@gmx.com>
2021-02-04 15:06:29 -0500

    Updating deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh tyler@5.196.14.162 'cd ~/joinlemmy-site && git pull --recurse-submodules && ./build.sh'
+ssh tyler@5.196.14.162 'cd ~/joinlemmy-site && git pull --recurse-submodules && bash -l build.sh'
