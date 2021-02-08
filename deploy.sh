1b7ee875f0d8c812407c3925ccf127aab56e3866
Dessalines <tyhou13@gmx.com>
2021-02-07 23:54:43 -0500

    Adding submodule update to deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+git submodule update --remote
+git add lemmy-docs
+git commit -m"Updating docs"
+git push
 ssh tyler@5.196.14.162 'cd ~/joinlemmy-site && git pull --recurse-submodules && bash -l build.sh'
