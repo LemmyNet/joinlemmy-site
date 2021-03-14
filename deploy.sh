c73bcd1a1a035728d48db48971ec9fb4937a151e
Dessalines <tyhou13@gmx.com>
2021-03-14 10:43:50 -0400

    Trying a first docker deploy.

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,5 @@
 
 new_tag="$1"
 
-# Old deploy
-# sudo docker build . --tag dessalines/lemmy-ui:$new_tag
-# sudo docker push dessalines/lemmy-ui:$new_tag
-
 git tag $new_tag
 git push origin $new_tag
