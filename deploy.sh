fe1c819163ba5b256e27176b1c5c94a7a6069836
Felix Ableitner <me@nutomic.com>
2021-04-21 14:02:41 +0200

    Add check for unused languages in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 git submodule update --remote
 git add lemmy-docs
@@ -6,6 +7,14 @@ git commit -m"Updating docs"
 git add joinlemmy-translations
 git commit -m"Updating translations"
 git push
+
+# look for unused translations
+for langfile in joinlemmy-translations/translations/*.json; do
+    lang=$(basename $langfile .json)
+    if ! grep -q "\"./translations/$lang\"" src/shared/i18next.ts; then
+      echo "Unused language $lang"
+    fi
+done
 
 new_tag="$1"
 
