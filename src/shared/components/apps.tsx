54e24669d2cd9a85b2335c756f773f91b20d9c73
Dessalines <dessalines@users.noreply.github.com>
2022-01-28 18:05:15 -0500

    Adding jerboa to apps. (#73)

--- src/shared/components/apps.tsx
+++ src/shared/components/apps.tsx
@@ -22,7 +22,7 @@ export class Apps extends Component<any, any> {
           <div class="row">
             <div class="card col-6">
               <AppDetails
-                name="lemmur"
+                name="Lemmur"
                 description="A Lemmy client for Android, Linux, and Windows."
                 link="https://github.com/LemmurOrg/lemmur"
                 icon="/static/assets/images/lemmur.svg"
@@ -33,8 +33,7 @@ export class Apps extends Component<any, any> {
                     icon: "f-droid",
                   },
                   {
-                    link:
-                      "https://play.google.com/store/apps/details?id=com.krawieck.lemmur",
+                    link: "https://play.google.com/store/apps/details?id=com.krawieck.lemmur",
                     icon: "googleplay",
                   },
                   {
@@ -47,15 +46,38 @@ export class Apps extends Component<any, any> {
 
             <div class="card col-6">
               <AppDetails
-                name="remmel"
+                name="Jerboa"
+                description="A native Android app made by Lemmy's developers"
+                link="https://github.com/dessalines/jerboa"
+                icon="/static/assets/images/jerboa.svg"
+                banner="/static/assets/images/jerboa_screen.webp"
+                links={[
+                  {
+                    link: "https://f-droid.org/en/packages/com.jerboa",
+                    icon: "f-droid",
+                  },
+                  {
+                    link: "https://play.google.com/store/apps/details?id=com.jerboa",
+                    icon: "googleplay",
+                  },
+                  {
+                    link: "https://github.com/dessalines/jerboa",
+                    icon: "github",
+                  },
+                ]}
+              />
+            </div>
+
+            <div class="card col-6">
+              <AppDetails
+                name="Remmel"
                 description="An iOS client for lemmy."
                 link="https://github.com/uuttff8/Lemmy-iOS"
                 icon="/static/assets/images/remmel.webp"
                 banner="/static/assets/images/remmel_screen.webp"
                 links={[
                   {
-                    link:
-                      "https://apps.apple.com/us/app/remmel-for-lemmy/id1547988171",
+                    link: "https://apps.apple.com/us/app/remmel-for-lemmy/id1547988171",
                     icon: "appleinc",
                   },
                   {
