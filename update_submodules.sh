401d4b3ce823e2fd9b389cad5450a75134ee0f3a
Nutomic <me@nutomic.com>
2022-05-17 13:53:57 +0000

    Update crawler to latest version (#103)

--- update_submodules.sh
+++ update_submodules.sh
@@ -11,11 +11,6 @@ git fetch weblate
 git merge weblate/main
 git push
 
-popd
-pushd ../lemmy-instance-stats
-./update.sh
-popd
-
 git submodule update --remote
 git add joinlemmy-translations
 git add lemmy-translations
