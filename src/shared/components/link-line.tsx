49818c012d5054d5537d0a2fba0de6c543100ba0
Nutomic <me@nutomic.com>
2023-04-25 22:40:09 +0200

    Move about page to documentation (#145)
    https://github.com/LemmyNet/lemmy-docs/pull/172#discussion_r1174154932

--- src/shared/components/link-line.tsx
+++ src/shared/components/link-line.tsx
@@ -12,7 +12,6 @@ export class LinkLine extends Component<any, any> {
       <>
         <Link to="/instances">{i18n.t("join")}</Link>
         <Link to="/news">{i18n.t("news")}</Link>
-        <Link to="/about">{i18n.t("about")}</Link>
         <Link to="/apps">{i18n.t("apps")}</Link>
         <Link to="/donate">{i18n.t("donate")}</Link>
         <a href={`/docs/${getDocsLanguage(i18n.language)}/index.html`}>
