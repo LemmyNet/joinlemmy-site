3d6972cee98a2ec62876703ad0b9b225e844c8ea
Dessalines <tyhou13@gmx.com>
2023-10-13 14:19:05 -0400

    Removing some useless instance helper links.

--- src/shared/components/instances-definitions.ts
+++ src/shared/components/instances-definitions.ts
@@ -9,20 +9,12 @@ export const INSTANCE_HELPERS: InstanceHelper[] = [
     link: "https://lemmyverse.net/communities",
   },
   {
-    name: "lemmy.fediverse.observer",
+    name: "Lemmy Fediverse Observer",
     link: "https://lemmy.fediverse.observer/list",
   },
   {
     name: "Awesome-Lemmy-Instances on GitHub",
     link: "https://github.com/maltfield/awesome-lemmy-instances",
-  },
-  {
-    name: "The-Federation.info Lemmy Instances Page",
-    link: "https://the-federation.info/platform/73",
-  },
-  {
-    name: "Feddit's Lemmymap",
-    link: "https://lemmymap.feddit.de/",
   },
   {
     name: "Feddit's Lemmy Community Browser",
