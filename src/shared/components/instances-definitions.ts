08f955ca8c87c68cbfbcc318f0f1f9c326c05cee
Chris <weiteck@users.noreply.github.com>
2023-11-07 07:48:13 +1100

    Update instances-definitions.ts (#270)
    Added leminal.space instance

--- src/shared/components/instances-definitions.ts
+++ src/shared/components/instances-definitions.ts
@@ -459,13 +459,11 @@ export const RECOMMENDED_INSTANCES: RecommendedInstance[] = [
     languages: ["eu"],
     topics: [TECHNOLOGY],
   },
-
   {
     domain: "tabinezumi.net",
     languages: ["ja"],
     topics: [TECHNOLOGY],
   },
-
   {
     domain: "lm.korako.me",
     languages: ["ja"],
@@ -495,5 +493,10 @@ export const RECOMMENDED_INSTANCES: RecommendedInstance[] = [
     domain: "lemmy.myserv.one",
     languages: ["en"],
     topics: [GENERAL],
+  },
+  {
+    domain: "leminal.space",
+    languages: ["en"],
+    topics: [GENERAL, ART, MUSIC],
   },
 ];
