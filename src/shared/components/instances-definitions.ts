cc999c7df6730f1d96efe9cbf062b94481bf6bd4
Nutomic <me@nutomic.com>
2024-03-12 16:09:13 +0100

    Remove link for awesome-lemmy-instances (#304)

--- src/shared/components/instances-definitions.ts
+++ src/shared/components/instances-definitions.ts
@@ -13,10 +13,6 @@ export const INSTANCE_HELPERS: InstanceHelper[] = [
     link: "https://lemmy.fediverse.observer/list",
   },
   {
-    name: "Awesome-Lemmy-Instances on GitHub",
-    link: "https://github.com/maltfield/awesome-lemmy-instances",
-  },
-  {
     name: "Feddit's Lemmy Community Browser",
     link: "https://browse.feddit.de/",
   },
