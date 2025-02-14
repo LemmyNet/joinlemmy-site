7822183be1db58f88e3cca481b85763a8ef4910d
Felix Ableitner <me@nutomic.com>
2025-02-14 14:34:51 +0100

    use jq for filtering

--- crawl.mjs
+++ crawl.mjs
@@ -22,21 +22,27 @@ try {
   // Run Rust crawler with given params. Then pipe output directly into jq, to filter
   // out fields with lots of data which we dont need. This is necessary because otherwise
   // Javascript may crash when loading the crawl output.
+  // Filters all instances with closed registrations or those with open registrations
+  // (often used by bots). Instances with few active users are also excluded.
   const run = spawn(
     "sh",
     [
       "-c",
-      `cargo run --release -- --json --start-instances ${all_recommended} \
+      `cargo run --release -- --max-crawl-distance 0 --json --start-instances ${all_recommended} \
       --exclude-instances ${recommended_instances.exclude} | \
       jq 'del(.instance_details[].federated_instances, \
         .instance_details[].site_info.all_languages, \
         .instance_details[].site_info.discussion_languages, \
-        .instance_details[].site_info.admins, .instance_details[].site_info.taglines, \
+        .instance_details[].site_info.admins, 
+        .instance_details[].site_info.taglines, \
         .instance_details[].site_info.custom_emojis, \
+        .instance_details[].site_info.site_view.site.public_key, \
         .instance_details[].site_info.site_view.local_site_rate_limit, \
         .instance_details[].site_info.site_view.local_site.application_question, \
         .instance_details[].site_info.site_view.local_site.legal_information, \
-        .instance_details[].site_info.site_view.local_site.slur_filter_regex)'`,
+        .instance_details[].site_info.site_view.local_site.slur_filter_regex)' | \
+        jq '.instance_details |= map(select(.site_info.site_view.local_site.registration_mode | contains ("RequireApplication")))' | \
+        jq '.instance_details |= map(select(.site_info.site_view.counts.users_active_month > ${min_monthly_users}))'`,
     ],
     {
       cwd: "lemmy-stats-crawler",
@@ -58,22 +64,7 @@ try {
 
   run.on("close", _exitCode => {
     var stats = JSON.parse(savedOutput);
-    // Crawl results from all instances include tons of data which needs to be compiled.
-    // If it is too much data it breaks the build, so we need to exclude as much as possible.
     stats.instance_details = stats.instance_details
-      // Exclude instances with closed registration
-      .filter(
-        i => i.site_info.site_view.local_site.registration_mode != "closed",
-      )
-      // Exclude instances with open registration (often used by bots
-      .filter(
-        i => i.site_info.site_view.local_site.registration_mode != "open",
-      )
-      // Exclude instances with few active users
-      .filter(
-        i =>
-          i.site_info.site_view.counts.users_active_month > min_monthly_users,
-      )
       // Exclude large instances which represent more than 30% of all active users
       .filter(i => {
         let active_users_percent =
