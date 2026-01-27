884c20b229f9696b7c399af7aa3066115f07b72e
Felix Ableitner <me@nutomic.com>
2026-01-27 15:07:08 +0100

    Move app images to subfolder, resize, remove unused

--- src/shared/components/app-definitions.ts
+++ src/shared/components/app-definitions.ts
@@ -101,8 +101,8 @@ const BLORP: AppDetails = {
   name: "Blorp",
   description: "A Threadiverse client for web, iOS, MacOS, and Android",
   link: "https://blorpblorp.xyz",
-  icon: "/static/assets/images/blorp.png",
-  banner: "/static/assets/images/blorp_screen.webp",
+  icon: "/static/assets/apps/blorp.png",
+  banner: "/static/assets/apps/blorp_screen.webp",
   links: {
     appleinc:
       "https://apps.apple.com/us/app/blorp-for-lemmy/id6739925430?platform=iphone",
@@ -120,8 +120,8 @@ const ECHO: AppDetails = {
   name: "Echo",
   description: "A fully native Lemmy Client for iOS",
   link: "https://echo.rrainn.com",
-  icon: "/static/assets/images/echo.webp",
-  banner: "/static/assets/images/echo_screen.webp",
+  icon: "/static/assets/apps/echo.webp",
+  banner: "/static/assets/apps/echo_screen.webp",
   links: {
     appleinc: "https://apps.apple.com/us/app/echo-for-lemmy/id6450902296",
   },
@@ -133,8 +133,8 @@ const VOYAGER: AppDetails = {
   name: "Voyager",
   description: "A Lemmy Client for iOS, Android and the web",
   link: "https://getvoyager.app",
-  icon: "/static/assets/images/voyager.png",
-  banner: "/static/assets/images/voyager_screen.webp",
+  icon: "/static/assets/apps/voyager.png",
+  banner: "/static/assets/apps/voyager_screen.webp",
   links: {
     appleinc:
       "https://apps.apple.com/us/app/voyager-for-lemmy/id6451429762?platform=iphone",
@@ -153,8 +153,8 @@ const THUNDER: AppDetails = {
   description:
     "An open-source cross-platform Lemmy client for iOS and Android built with Flutter",
   link: "https://github.com/thunder-app/thunder",
-  icon: "/static/assets/images/thunder_logo.webp",
-  banner: "/static/assets/images/thunder_screen.webp",
+  icon: "/static/assets/apps/thunder_logo.webp",
+  banner: "/static/assets/apps/thunder_screen.webp",
   links: {
     fdroid: "https://apt.izzysoft.de/fdroid/index/apk/com.hjiangsu.thunder",
     appleinc: "https://apps.apple.com/iq/app/thunder-for-lemmy/id6450518497",
@@ -170,8 +170,8 @@ const JERBOA: AppDetails = {
   name: "Jerboa",
   description: "A native Android app made by Lemmy's developers",
   link: "https://github.com/LemmyNet/jerboa",
-  icon: "/static/assets/images/jerboa.svg",
-  banner: "/static/assets/images/jerboa_screen.webp",
+  icon: "/static/assets/apps/jerboa.svg",
+  banner: "/static/assets/apps/jerboa_screen.webp",
   links: {
     fdroid: "https://f-droid.org/en/packages/com.jerboa",
     googleplay: "https://play.google.com/store/apps/details?id=com.jerboa",
@@ -185,8 +185,8 @@ const ETERNITY: AppDetails = {
   name: "Eternity",
   description: "A Lemmy client for Android written in Java.",
   link: "https://codeberg.org/Bazsalanszky/Eternity",
-  icon: "/static/assets/images/eternity_icon.webp",
-  banner: "/static/assets/images/eternity_screen.webp",
+  icon: "/static/assets/apps/eternity_icon.webp",
+  banner: "/static/assets/apps/eternity_screen.webp",
   links: {
     fdroid: "https://f-droid.org/en/packages/eu.toldi.infinityforlemmy",
     googleplay:
@@ -202,8 +202,8 @@ const BOOST: AppDetails = {
   name: "Boost for Lemmy",
   description: "A smooth app for Lemmy.",
   link: "https://play.google.com/store/apps/details?id=com.rubenmayayo.lemmy",
-  icon: "/static/assets/images/boost_icon.webp",
-  banner: "/static/assets/images/boost_screen.webp",
+  icon: "/static/assets/apps/boost_icon.webp",
+  banner: "/static/assets/apps/boost_screen.webp",
   links: {
     googleplay:
       "https://play.google.com/store/apps/details?id=com.rubenmayayo.lemmy",
@@ -216,8 +216,8 @@ const SYNC: AppDetails = {
   name: "Sync for Lemmy",
   description: "A full-featured app for browsing Lemmy on the go.",
   link: "https://play.google.com/store/apps/details?id=io.syncapps.lemmy_sync",
-  icon: "/static/assets/images/sync_icon.webp",
-  banner: "/static/assets/images/sync_screen.webp",
+  icon: "/static/assets/apps/sync_icon.webp",
+  banner: "/static/assets/apps/sync_screen.webp",
   links: {
     googleplay:
       "https://play.google.com/store/apps/details?id=io.syncapps.lemmy_sync",
@@ -230,8 +230,8 @@ const MLEM: AppDetails = {
   name: "Mlem",
   description: "An open-source Lemmy client for iOS, written in Swift.",
   link: "https://github.com/mlemgroup/mlem",
-  icon: "/static/assets/images/mlem.png",
-  banner: "/static/assets/images/mlem_screen.webp",
+  icon: "/static/assets/apps/mlem.png",
+  banner: "/static/assets/apps/mlem_screen.webp",
   links: {
     appleinc: "https://apps.apple.com/us/app/mlem-for-lemmy/id6450543782",
     github: "https://github.com/mlemgroup/mlem",
@@ -244,7 +244,7 @@ const LEMMY_UI: AppDetails = {
   name: "lemmy-ui",
   description: "The official web app for lemmy.",
   link: "https://github.com/LemmyNet/lemmy-ui",
-  banner: "/static/assets/images/lemmy_ui.webp",
+  banner: "/static/assets/apps/lemmy_ui.webp",
   links: {
     github: "https://github.com/LemmyNet/lemmy-ui",
   },
@@ -256,8 +256,8 @@ const PHOTON: AppDetails = {
   name: "Photon",
   description: "A fully-featured, intuitive and clean web app for Lemmy.",
   link: "https://github.com/Xyphyn/photon",
-  banner: "/static/assets/images/photon.webp",
-  icon: "/static/assets/images/photon-logo.svg",
+  banner: "/static/assets/apps/photon.webp",
+  icon: "/static/assets/apps/photon-logo.svg",
   links: {
     web: "https://phtn.app/",
     github: "https://github.com/Xyphyn/photon",
@@ -271,8 +271,8 @@ const ALEXANDRITE: AppDetails = {
   description:
     "A beautiful and convenient desktop-first alternate web UI for Lemmy.",
   link: "https://github.com/sheodox/alexandrite",
-  icon: "/static/assets/images/alexandrite_logo.svg",
-  banner: "/static/assets/images/alexandrite_screen.webp",
+  icon: "/static/assets/apps/alexandrite_logo.svg",
+  banner: "/static/assets/apps/alexandrite_screen.webp",
   links: {
     github: "https://github.com/sheodox/alexandrite",
     web: "https://a.lemmy.world/lemmy.world",
@@ -285,7 +285,7 @@ const MLMYM: AppDetails = {
   name: "mlmym",
   description: "A familiar desktop experience for lemmy",
   link: "https://github.com/rystaf/mlmym",
-  banner: "/static/assets/images/mlmym_screen.webp",
+  banner: "/static/assets/apps/mlmym_screen.webp",
   links: {
     github: "https://github.com/rystaf/mlmym",
     web: "https://old.lemmy.world/",
@@ -298,7 +298,7 @@ const NEONMODEM: AppDetails = {
   name: "neonmodem",
   description: "BBS-style TUI client",
   link: "https://github.com/mrusme/neonmodem",
-  banner: "/static/assets/images/neonmodem.webp",
+  banner: "/static/assets/apps/neonmodem.webp",
   links: {
     github: "https://github.com/mrusme/neonmodem",
   },
@@ -310,8 +310,8 @@ const SUMMIT: AppDetails = {
   name: "Summit",
   description: "A Lemmy Client for Android.",
   link: "https://play.google.com/store/apps/details?id=com.idunnololz.summit",
-  icon: "/static/assets/images/summit_logo.svg",
-  banner: "/static/assets/images/summit_screen.webp",
+  icon: "/static/assets/apps/summit_logo.svg",
+  banner: "/static/assets/apps/summit_screen.webp",
   links: {
     googleplay:
       "https://play.google.com/store/apps/details?id=com.idunnololz.summit",
@@ -326,8 +326,8 @@ const ARCTIC: AppDetails = {
   name: "Arctic",
   description: "A free and native iOS client, written in Swift.",
   link: "https://lemmy.world/c/arctic",
-  icon: "/static/assets/images/arctic_icon.webp",
-  banner: "/static/assets/images/arctic_screen.webp",
+  icon: "/static/assets/apps/arctic_icon.webp",
+  banner: "/static/assets/apps/arctic_screen.webp",
   links: {
     appleinc: "https://apps.apple.com/us/app/arctic-for-lemmy/id6457925837",
   },
